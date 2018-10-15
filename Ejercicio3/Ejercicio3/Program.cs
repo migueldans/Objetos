9eebb6210ae776fda7e535d2b0279e5cd748fd6c
migueldans <39558093+migueldans@users.noreply.github.com>
2018-10-15 16:47:15 +0200

    commit

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Satelite s1 = new Satelite(1000,1000,1000);
+            Console.ReadLine();
+
+           
+            s1.VariaAltura(10);
+            Console.ReadLine();
+
+            
+            if(s1.EnOrbita() ==true)
+            {
+                Console.WriteLine("En el espacio");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("En tierra");
+                Console.ReadLine();
+            }
+
+            s1.VariaPosicion(20,30);
+            Console.ReadLine();
+            
+
+            s1.PrintPosicion();
+
+            
+
+            
+
+
+        }
+    }
+}
---
+++ Ejercicio3/Ejercicio3/satelite.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    public class Satelite
+    {
+        private double meridiano;
+        private double paralelo;
+        private double distancia_tierra;
+        
+        public Satelite(double m, double p, double d)
+        {
+            meridiano = m;
+            paralelo = p;
+            distancia_tierra = d;
+        }
+
+        public Satelite()
+        {
+            meridiano = paralelo = distancia_tierra = 0;
+        }
+
+        public void SetPosicion(double m, double p, double d)
+        {
+            meridiano = m;
+            paralelo = p;
+            distancia_tierra = d;
+        }
+
+        public void PrintPosicion()
+        {
+            Console.WriteLine("El satélite se encuentra en el paralelo " + paralelo + "Meridiano " + meridiano + " a una distancia de la tierra de "+distancia_tierra+"Kilómetros");
+            Console.ReadLine();
+        }
+
+        public void VariaAltura(double des)
+        {
+            distancia_tierra = distancia_tierra + des;
+        }
+    
+        public bool EnOrbita()
+        {
+
+            if (distancia_tierra > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void VariaPosicion(double variap, double variam)
+        {
+            
+            meridiano = meridiano + variam;
+            paralelo = paralelo + variap;
+
+        }
+
+    }
+
+}
