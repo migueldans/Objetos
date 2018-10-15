728d5df4759ef074331de645a8d48d47e738b014
migueldans <39558093+migueldans@users.noreply.github.com>
2018-10-15 13:32:44 +0200

    commit

---
+++ finanzas/finanzas/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finanzas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Finanzas f1 = new Finanzas();
+            Console.WriteLine("5 dolares son " + f1.DolarsToEuros(5) + " euros");
+            Console.WriteLine("5 euros son " + f1.EurosToDolars(5) + " dolares");
+            Console.ReadLine();
+           
+
+            Finanzas f2 = new Finanzas(0.99);
+            //f2.SetCambio(0.99); se pone aqui o sino despues de finanzas como está puesto
+            Console.WriteLine("10 dolares son " + f2.DolarsToEuros(10) + " euros");
+            Console.WriteLine("10 euros son " + f2.EurosToDolars(10) + " dolares");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ finanzas/finanzas/finanzas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finanzas
+{
+    class Finanzas
+    {
+        ////Realiza una clase finanzas que convierta dólares a euros y viceversa.
+            
+        //private double dinero;
+        private double cambio;
+
+        ////La clase tiene que tener:
+
+        ////Un constructor finanzas() por defecto, el cual establecerá el cambio Dólar-Euro en 0.92 .
+
+        ////Un constructor finanzas(double), el cual permitirá configurar el cambio dólar-euro.
+
+        public Finanzas()
+        {
+              this.cambio = 0.92;
+        }
+
+        public Finanzas(double cambio)
+        {
+            this.cambio = cambio;
+        }
+
+        ////Codifica los métodos dolaresToEuros y eurosToDolares.
+
+        //public double GetDinero()
+        //{
+        //    return dinero;
+        //}
+
+        //public void SetDinero(double dinero)
+        //{
+        //    this.dinero = dinero;
+        //}
+
+        public double GetCambio()
+        {
+            return cambio;
+        }
+
+        public void SetCambio(double cambio)
+        {
+            this.cambio = cambio;
+        }
+
+
+        public double DolarsToEuros(double cantidad)
+        {
+            return cantidad * cambio;
+        }
+
+        public double EurosToDolars(double cantidad)
+        {
+            return cantidad / cambio;
+        }
+
+        //Prueba que dicha clase funciona correctamente haciendo conversiones entre euros y dólares.
+
+
+
+
+    }
+}
